571b0d4de634b1a6087c29e96b55be76a2cefeeb
ibrunera <bteixeira010@gmail.com>
2021-01-12 18:51:07 -0300

    appointments route get n post

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,26 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
+const appointments = [];
+
 appointmentsRouter.post('/', (request, response) => {
-  return response.json({ message: 'Hello Barber' });
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+appointmentsRouter.get('/', (request, response) => {
+  return response.json(appointments);
 });
 
 export default appointmentsRouter;
