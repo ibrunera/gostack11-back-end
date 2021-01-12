cb581d92b4a717fb7e8126c7c2ccf71d962a9557
ibrunera <bteixeira010@gmail.com>
2021-01-12 18:36:20 -0300

    appointments route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
 import express from 'express';
 import routes from './routes';
-import './database';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log('Server runing at http://localhost:3333');
-});
+app.listen(3333);
