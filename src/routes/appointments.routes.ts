516d9d100a3c18a81e3d2bd82aef7d4c48f59fa5
ibrunera <bteixeira010@gmail.com>
2021-01-19 16:59:28 -0300

    model Appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -28,11 +22,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'Appointment already booked.' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, date);
 
   appointments.push(appointment);
 
