5895b31d8abbb4005a7c380d75aa48c13868bce7
ibrunera <bteixeira010@gmail.com>
2021-01-18 21:21:44 -0300

    validating date

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,15 +1,32 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual, parse } from 'date-fns';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
+
+  // false se n encontrar.
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'Appointment already booked.' });
+  }
 
   const appointment = {
     id: uuid(),
